42be9ff10f0cc016967750ad2fa2f67c66e58773
Pearlie Tan <69686966+tingxtan@users.noreply.github.com>
2020-10-31 20:41:13 +0800

    Add files via upload

---
+++ build/precache-manifest.a3eb6249bbe3b0f25a09bafb8b7b94bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5a5dcefea3508d66a7f2cd860df6b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a1889d8062682cb8c1f",
+    "url": "/static/css/main.5e978fad.chunk.css"
+  },
+  {
+    "revision": "08cdc02b205903e16ea5",
+    "url": "/static/js/2.197e0aac.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/static/js/2.197e0aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1889d8062682cb8c1f",
+    "url": "/static/js/main.49637168.chunk.js"
+  },
+  {
+    "revision": "6ca49defa095afd3e9c1",
+    "url": "/static/js/runtime-main.f9911b94.js"
+  }
+]);
