75883d3ef44dfb5e10c6fe8c441a8d928d7ecefb
Pearlie Tan <69686966+tingxtan@users.noreply.github.com>
2020-10-31 20:34:56 +0800

    Add files via upload

---
+++ src/Sidebar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import SurroundSoundOutlinedIcon from '@material-ui/icons/SurroundSoundOutlined';
+import { getTokenFromResponse } from "./spotify";
+import { useStateValue } from "./StateProvider";
+
+function Sidebar() {
+  const [{ playlists }, dispatch] = useStateValue();
+  console.log(playlists);
+
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SidebarOption Icon={HomeIcon} option="Home" />
+      <SidebarOption Icon={SearchIcon} option="Browse" />
+      <SidebarOption Icon={SurroundSoundOutlinedIcon} option="Radio" />
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+      {playlists?.items?.map((playlist) => (
+        <SidebarOption option={playlist.name} />
+      ))}
+    </div>
+  );
+}
+
+export default Sidebar;
